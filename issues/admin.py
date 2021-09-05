46115256e5faf360420b3e9edb98a08a59da2003
AO <ao@post.cz>
2021-09-05 21:38:30 +0200

    Cosmetic changes in Django Admin

--- issues/admin.py
+++ issues/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import State, Issue, Category
 
-# Register your models here.
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'creator', 'responsible_person', 'description', 'state', 'category', 'created_at',
+                    'finished_at',)
+    exclude = ('duration',)
+
+    # fill creator and responsible_person fields as a current user
+    def get_changeform_initial_data(self, request):
+        get_data = super(IssueAdmin, self).get_changeform_initial_data(request)
+        get_data['creator'] = request.user.pk
+        get_data['responsible_person'] = request.user.pk
+        return get_data
+
+
+admin.site.register(State)
+admin.site.register(Category)
+admin.site.register(Issue, IssueAdmin)
+admin.site.unregister(Group)
+admin.site.site_title = 'Simple Tracker'
+admin.site.index_title = 'Tracker Administration'
+admin.site.site_url = '/admin'
