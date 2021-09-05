88044376ae01134bf7abedddf0fff898c9fcfa6f
AO <ao@post.cz>
2021-09-05 22:11:04 +0200

    Fix: return '-' values for issues statistics if no issues were finished yet

--- issues/templatetags/custom_filters.py
+++ issues/templatetags/custom_filters.py
@@ -8,13 +8,18 @@ register = template.Library()
 
 
 def calculate_avg_max_min_duration(durations_list):
-    max_duration_timedelta, min_duration_timedelta = max(durations_list), min(durations_list)
-    sum_timedelta = sum(durations_list, timedelta())
-    avg_duration_timedelta = sum_timedelta / len(durations_list)
-    # clear microseconds
-    max_duration = timedelta(days=max_duration_timedelta.days, seconds=max_duration_timedelta.seconds)
-    min_duration = timedelta(days=min_duration_timedelta.days, seconds=min_duration_timedelta.seconds)
-    avg_duration = timedelta(days=avg_duration_timedelta.days, seconds=avg_duration_timedelta.seconds)
+    if durations_list:
+        max_duration_timedelta, min_duration_timedelta = max(durations_list), min(durations_list)
+        sum_timedelta = sum(durations_list, timedelta())
+        avg_duration_timedelta = sum_timedelta / len(durations_list)
+        # clear microseconds
+        max_duration = timedelta(days=max_duration_timedelta.days, seconds=max_duration_timedelta.seconds)
+        min_duration = timedelta(days=min_duration_timedelta.days, seconds=min_duration_timedelta.seconds)
+        avg_duration = timedelta(days=avg_duration_timedelta.days, seconds=avg_duration_timedelta.seconds)
+    else:
+        max_duration = '-'
+        min_duration = '-'
+        avg_duration = '-'
     return (str(avg_duration).replace('days,', 'dnů a '), str(max_duration).replace('days,', 'dnů a '),
             str(min_duration).replace('days,', 'dnů a '))
 
